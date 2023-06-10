2bfc6ed9e2fddea60cdef83c4e7ae3e02b65425f
vinimari <viniciusmarinho44@hotmail.com>
2023-06-10 20:09:47 -0300

    fix: classes directory

---
+++ lib/classes/Itens/ReadItem.java
@@ -0,0 +1,16 @@
+package classes.Itens;
+
+public class ReadItem extends Item {
+
+  ReadItem(int id, String item_name, int[] current_position) {
+    super(id, item_name, current_position);
+    //TODO Auto-generated constructor stub
+  }
+
+  @Override
+  protected void onUse() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'onUse'");
+  }
+  
+}
